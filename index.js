66f7726dc098008ecba32c58569a498317399b3a
Bhavesh <bhavesh@valura.ai>
2026-04-01 09:15:25 +0530

    chore: update email POST handler to use async/await and restructure payload

--- index.js
+++ index.js
@@ -5,15 +5,20 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 const upload = multer();
 
-app.post("/email", upload.any(), (req, res) => {
-  const { from, subject, text, html } = req.body;
-  console.log("From:", from);
-  console.log("Subject:", subject);
-  console.log("Body:", text);
+app.post("/email", async (req, res) => {
+  // const { from, subject, text, html } = req.body;
+
+  const payload = {
+    text : req.body.email
+  }
+  const result = await fetch("https://ml-model-us52.onrender.com/detech", {
+    method : "POST",
+    body : JSON.stringify(payload)
+  });
 
   // 👉 Add your tagging logic here
-
-  res.status(200).send("OK");
+  console.log(result);
+  res.status(200).json("OK");
 });
 
 app.listen(3000, () => console.log("Server running"));
