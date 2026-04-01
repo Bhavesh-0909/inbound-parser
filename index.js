d761f582ed4a0271bf02d79168b832b5a6239b61
Bhavesh <bhavesh@valura.ai>
2026-04-01 09:25:23 +0530

    chore: update email POST handler to include multer for file uploads and wrap logic in try-catch

--- index.js
+++ index.js
@@ -5,25 +5,31 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 const upload = multer();
 
-app.post("/email", async (req, res) => {
-  const { from, to, subject, html, email } = req.body;
-  console.log("from", from),
-  console.log("to", to);
-  console.log("subject", subject);
-  console.log("html", html);
-  console.log("email", email);
+app.post("/email", upload.any(), async (req, res) => {
+  try {
+    const { from, to, subject, html, email } = req.body;
+    console.log("from", from),
+    console.log("to", to);
+    console.log("subject", subject);
+    console.log("html", html);
+    console.log("email", email);
 
-  const payload = {
-    text : email
+    const payload = {
+      text : email
+    }
+    const result = await fetch("https://ml-model-us52.onrender.com/detech", {
+      method : "POST",
+      body : JSON.stringify(payload)
+    });
+
+    // 👉 Add your tagging logic here
+    console.log(result);
+    res.status(200).json("OK");
+    
+  } catch (error) {
+    console.error(error);
   }
-  const result = await fetch("https://ml-model-us52.onrender.com/detech", {
-    method : "POST",
-    body : JSON.stringify(payload)
-  });
-
-  // 👉 Add your tagging logic here
-  console.log(result);
-  res.status(200).json("OK");
+  
 });
 
 app.listen(3000, () => console.log("Server running"));
